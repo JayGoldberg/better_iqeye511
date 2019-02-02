ffa3b5c1678fd38cfee64b1a09c47de456464676
JayGoldberg <jaymgoldberg@gmail.com>
2019-02-02 12:56:50 -0800

    A few more JS run through js-beautify
    grep js ./*.html.dp
    
    which reveals which JS is included in what HTML pages...in addition
    to $INC() which is used to include templated js.dp files.
    
    Exclude cameo* pages as I don't use this licensed IQinvision feature.

--- rom/web/shared/date_time.js
+++ rom/web/shared/date_time.js
@@ -1,27 +1,24 @@
-
-
-
 function isADate(field) {
     if (field.type != "text") return false;
-    
+
     var exp = new RegExp("(\\d+)\\/(\\d+)\\/(\\d+)$$");
     var arr = field.value.match(exp);
-    if (arr == null) return false; 
-    if ((arr[1]<1) || (arr[1]> 12) || (arr[2]<1) || (arr[2]>31)) {
-	return false;
+    if (arr == null) return false;
+    if ((arr[1] < 1) || (arr[1] > 12) || (arr[2] < 1) || (arr[2] > 31)) {
+        return false;
     }
     return true;
 }
 
 
-function isATime(field) { 
+function isATime(field) {
     if (field.type != "text") return false;
     var exp = new RegExp("(\\d+):(\\d+):(\\d+)$$");
     var arr = field.value.match(exp);
-    if (arr == null) return false; 
-    if ((arr[1]<0) || (arr[2]<0) || (arr[3]<0) ||
-	(arr[1]>24) || (arr[2]>60) || (arr[3]>60)){
-	return false;
+    if (arr == null) return false;
+    if ((arr[1] < 0) || (arr[2] < 0) || (arr[3] < 0) ||
+        (arr[1] > 24) || (arr[2] > 60) || (arr[3] > 60)) {
+        return false;
     }
     return true;
 }
@@ -32,9 +29,9 @@ function getDateString() {
     var y = now.getYear();
     var d = now.getDate();
     var m = now.getMonth() + 1;
-    if (y<1000) y += 1900;	
-    if (m<10) m = "0" + m;
-    if (d<10) d = "0" + d;
+    if (y < 1000) y += 1900;
+    if (m < 10) m = "0" + m;
+    if (d < 10) d = "0" + d;
     return (m + "/" + d + "/" + y);
 }
 
@@ -50,9 +47,9 @@ function getTimeString() {
     var h = now.getHours();
     var m = now.getMinutes();
     var s = now.getSeconds();
-    if (h<10) h = "0" + h;
-    if (m<10) m = "0" + m;
-    if (s<10) s = "0" + s;
+    if (h < 10) h = "0" + h;
+    if (m < 10) m = "0" + m;
+    if (s < 10) s = "0" + s;
     return (h + ":" + m + ":" + s);
 }
 
@@ -61,5 +58,3 @@ function setDefaultTime() {
     document.myform["OidTB2.18"].value = getTimeString();
     setEcam(document.myform["OidTB2.18"]);
 }
-
-
